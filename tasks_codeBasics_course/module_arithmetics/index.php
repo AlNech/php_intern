677527969e8e5967cb744c72da1e5a604c4eaeb3
hikaRU-135 <hikari.desu13@gmail.com>
2023-01-26 22:04:26 +0400

    dev module with arithmetics and str php

---
+++ tasks_codeBasics_course/module_arithmetics/index.php
@@ -0,0 +1,9 @@
+<?php   
+
+    print_r(81 / 9);
+    print_r(6 - -81);
+    print_r(3 ** 5);
+    print_r(-8 / -4);
+    print_r(8 / 2 + 5 - -3 / 2);
+    print_r(70 * (3 + 4) / (8 + 2));
+    print_r(5 ** 2 - 3 * 7);
---
+++ tasks_codeBasics_course/module_str/index.php
@@ -0,0 +1,4 @@
+<?php
+    print_r("\"Khal Drogo's favorite word is \"athjahakar\"\"");
+    print_r("- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".");
+    print_r("Winter came for" . " the House of Frey.");
