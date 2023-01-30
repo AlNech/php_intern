c407771c7d72b7ac168e526592ea0f297d563772
hikaRU-135 <hikari.desu13@gmail.com>
2023-01-30 20:25:06 +0400

    dev module condition

---
+++ tasks_codeBasics_course/module_condition/index.php
@@ -0,0 +1,52 @@
+<?php
+    //task 1
+    function guessNumber($num){
+        if ($num === 42)
+            return 'You win!';
+            
+         return 'Try again!';   
+        
+    }
+
+    //task 2
+    function normalizeUrl($url){
+        $pos = strpos($url, 'http://' );
+        if ($pos !== false){
+            $url = substr ($url, 7, strlen($url));
+            return  'https://' . $url;
+        } else{
+            return 'https://' . $url;
+        }
+        $pos = strpos($url, 'https://' );
+        if ($pos !== false){
+            return  $url;
+        }
+    }
+
+    //task 3
+    function whoIsThisHouseToStarks($family){
+        if ($family === 'Karstark' || $family === 'Tully')
+            return 'friend';
+        if ($family === 'Lannister' || $family === 'Frey')
+            return 'enemy';
+        return 'neutral' ;       
+    }
+
+    //task 4
+    function flipFlop($text){
+        return ($text === 'flip') ? 'flop' : 'flip';
+    }
+
+    //task 5
+    function getNumberExplanation($text){
+        switch ($text){
+            case 666 :
+                return 'devil number';
+            case 42:
+                return 'answer for everything';
+            case 7:
+                return 'prime number';
+            default:
+                return null;            
+        }
+    }
