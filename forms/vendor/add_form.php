90ecf6ebdc68ec262e97ef438466ad5523aa34d1
hikaRU-135 <hikari.desu13@gmail.com>
2023-02-19 17:48:24 +0400

    fix bugs

--- forms/vendor/add_form.php
+++ forms/vendor/add_form.php
@@ -7,8 +7,14 @@
         $gender =  $_POST['gender'];
         $lang_prog =  $_POST['lang_prog'];
         $country = $_POST['country'];
-        $comment =  $_POST['comment'];
+        $comment =  htmlspecialchars($_POST['comment']);
 
-        mysqli_query($connect, "INSERT INTO `add_form`(`id`, `age`, `gender`, `lang_prog`, `country`, `comment`) VALUES (0,'$age','$gender','$lang_prog','$country','$comment')");
+
+        $lang = "";
+        foreach ($lang_prog as $row){
+            $lang .= $row . " ";
+        }
+
+        mysqli_query($connect, "INSERT INTO `add_form`(`id`, `age`, `gender`, `lang_prog`, `country`, `comment`) VALUES (0,'$age','$gender','$lang','$country','$comment')");
         header("Location: ../index.php");
    
